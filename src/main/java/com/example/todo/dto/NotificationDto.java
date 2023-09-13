82cf575eb77689dc3097053f57ec4a0b3aaa539d
BlackHan26 <128695414+BlackHan26@users.noreply.github.com>
2023-09-13 15:30:19 +0900

    feat: fix NotificationController, NotificationService * Service에서 주기적으로 업무상태 확인 후 알림기능 추가 * 댓글작성시 알림 받는 사람 변경 * MemberRepository 에 findAllByTeamId 추가

--- src/main/java/com/example/todo/dto/NotificationDto.java
+++ src/main/java/com/example/todo/dto/NotificationDto.java
@@ -8,12 +8,10 @@ import java.time.LocalDateTime;
 @Data
 public class NotificationDto {
     private String content;
-    private LocalDateTime createdTime;
 
     public static NotificationDto fromEntity(NotificationEntity entity) {
         NotificationDto notificationDto = new NotificationDto();
         notificationDto.setContent(entity.getContent());
-        notificationDto.setCreatedTime(entity.getCreatedTime());
         return notificationDto;
     }
 }
