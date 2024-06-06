9011a497823af8909d903f51e20b9b58a0866c86
acycoco <pink10257@naver.com>
2024-06-06 17:52:10 +0900

    refactor(TeamEntity): SOFT DELETE, setter 제거, 빌더 추가, 기본생성자 PROTECTED 추가

--- src/main/java/com/example/todo/domain/entity/TeamEntity.java
+++ src/main/java/com/example/todo/domain/entity/TeamEntity.java
@@ -2,15 +2,17 @@ package com.example.todo.domain.entity;
 
 import com.example.todo.domain.entity.user.User;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE todo.team SET deleted_at = current_timestamp where id = ?")
+@Where(clause = "deleted_at is null")
 @Getter
-@Setter
 public class TeamEntity extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,10 +31,12 @@ public class TeamEntity extends BaseTimeEntity {
     private User manager;
 
     @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
-    private List<MemberEntity> members;
+    private List<MemberEntity> members = new ArrayList<>();
 
+    public boolean validateJoinCode(String joinCode){
+        return this.getJoinCode().equals(joinCode);
+    }
 
-   
     public Long getManagerId() {
         return manager.getId();
     }
@@ -41,5 +45,15 @@ public class TeamEntity extends BaseTimeEntity {
         for (MemberEntity member : members) membersNamesList.add(member.getUser().getUsername());
 
         return membersNamesList;
+    }
+
+    @Builder
+    public TeamEntity(final String name, final String description, final String joinCode, final Integer participantNum, final Integer participantNumMax, final User manager) {
+        this.name = name;
+        this.description = description;
+        this.joinCode = joinCode;
+        this.participantNum = participantNum;
+        this.participantNumMax = participantNumMax;
+        this.manager = manager;
     }
 }
